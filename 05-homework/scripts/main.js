19933c14f119a908d3c2c05e80285a9fb9bb4686
mehrdadmg <m.majidzadeh.g@gmail.com>
2020-08-12 09:59:19 +0300

    deleted 05-homework from master

--- 05-homework/scripts/main.js
+++
@@ -1,7 +0,0 @@
-function changeScript(elemId) {
-  var Script = document.createElement('script');
-  Script.textContent = document.getElementById(elemId).innerText;
-  document.getElementsByTagName('head')[0].appendChild(Script);
-  document.getElementById(elemId).appendChild(Script)
-  Script.replaceWith(...document.getElementById(elemId).childNodes);
-}
